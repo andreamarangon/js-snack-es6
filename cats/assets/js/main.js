f99b9daf12cad7e44d984ab20a2c1be55d54e902
Andrea Marangon <marangonandrea84@gmail.com>
2021-03-20 18:00:51 +0100

    ESERCIZIO TERMINATO

--- cats/assets/js/main.js
+++ cats/assets/js/main.js
@@ -1,6 +1,5 @@
-console.log('ok');
 $(document).ready(function () {
-  //Milestone 1
+  //MILESTONE 1
 
   //Definire un array di oggetti; ogni oggetto rappresenta un gatto,
   //che è caratterizzato da: nome, età, colore e sesso.
@@ -9,92 +8,85 @@ $(document).ready(function () {
       nome: 'Mafalda',
       eta: 12,
       colore: 'nero',
-      sesso: 'femmina'
+      sesso: 'femmina',
     },
     {
       nome: 'Cesare',
       eta: 3,
       colore: 'rosso',
-      sesso: 'maschio'
+      sesso: 'maschio',
     },
     {
       nome: 'Morgana',
       eta: 7,
       colore: 'grigio',
-      sesso: 'femmina'
+      sesso: 'femmina',
     },
     {
       nome: 'Anselmo',
       eta: 20,
       colore: 'bianco',
-      sesso: 'maschio'
+      sesso: 'maschio',
     },
   ];
   // Tramite la funzione .forEach(), stampare in pagina tutti i gattini,
   // ciascuno con il proprio colore e il proprio nome.
   cats.forEach((item) => {
-    console.log(`${item.nome} è di colore ${item.colore}`);
     $('#cats').append(`<li>${item.nome} è di colore ${item.colore}</li>`)
   });
 
-  //Milestone 2
+  //MILESTONE 2
 
   // Dividere i gatti in due contenitori distinti in base al sesso
   const maschi = cats.filter((element) => element.sesso == 'maschio');
   const femmine = cats.filter((element) => element.sesso == 'femmina');
-  console.log(maschi);
-  console.log(femmine);
+
   //aggiungere a fianco di ogni gattino un fiocco colorato di rosa, se femmina, o di blu, se maschio.
   //Il colore del fiocco deve essere più tenue se il gatto è più giovane,
   //più scuro se il gatto è più vecchio.
-
+  const opacityMaschi = [];
   maschi.forEach((item) => {
-    $('#cats').append(`<li class='azzurro'>${item.nome} è di colore ${item.colore} <i class="fas fa-ribbon"></i></li>`)
-    // if (item.eta < 10) {
-    //   $('li.azzurro i').addClass('young')
-    // } else {
-    //   $('li.azzurro i').addClass('old')
-    // }
-  });
-
-
-  femmine.forEach((item) => {
-    $('#cats').append(`<li class='rosa'>${item.nome} è di colore ${item.colore} <i class="fas fa-ribbon"></i></li>`)
-    // if (item.eta < 10) {
-    //   $('li.rosa i').addClass('young')
-    // } else {
-    //   $('li.rosa i').addClass('old')
-    // }
-  });
-
-  //Il colore del fiocco deve essere più tenue se il gatto è più giovane,
-  //più scuro se il gatto è più vecchio.
-  cats.forEach((item) => {
+    let copyitem;
     if (item.eta < 10) {
-      $('li i').addClass('young');
-    } else if (item.eta >= 10){
-      $('li i').addClass('old');
+      copyitem = {... item, opacity: 1};
+    } else if (item.eta >= 10) {
+      copyitem ={... item, opacity: 0.5};
     }
-    console.log(item.eta);
+    $('#cats-opacity').append(`<li class="azzurro">${item.nome} è di colore ${item.colore} <i style = "opacity:${copyitem.opacity};" class="fas fa-ribbon"></i> </li>`)
+    opacityMaschi.push(copyitem);
 
   });
-  // cats.forEach((item) => {
-  //   let colore;
-  //
-  //   if(item.sesso == 'maschio') {
-  //       colore = 'azzurro';
-  //   }
-  //
-  //   else if(item.sesso == 'femmina') {
-  //       colore = 'rosa';
-  //   }
-  //
-  //   $('#cats').append(`<li class="${colore}">${item.nome} è di colore ${item.colore}<i class="fas fa-ribbon"></i></li>`)
-  // });
 
+  const opacityFemmine = [];
+  femmine.forEach((item) => {
+    let copyitem;
+    if (item.eta < 10) {
+      copyitem = {... item, opacity: 1};
+    } else if (item.eta >= 10) {
+      copyitem ={... item, opacity: 0.5};
+    }
+    $('#cats-opacity').append(`<li class="rosa">${item.nome} è di colore ${item.colore} <i style = "opacity:${copyitem.opacity};" class="fas fa-ribbon"></i> </li>`)
+    opacityFemmine.push(copyitem);
 
+  });
 
+  // MILESTONE 3
 
+  // Creare un nuovo array con prima tutti i gattini femmina e poi tutti i gattini maschio,
+  const catsOrder = [...opacityFemmine, ...opacityMaschi];
+  // inserendo solamente nome e colore e colore e opacità del fiocco per ogni gatto.
+  //con .map
+  newcatsOrder = catsOrder.map((item)=>{
+    const{nome, colore, opacity}=item;
+    return {nome, colore, opacity};
+  });
+  console.log(newcatsOrder);
 
+  //metodo alternativo con la cancellazione della proprietà tramite delete
+  // for (var i = 0; i < catsOrder.length; i++) {
+  //   delete catsOrder[i].eta;
+  //   delete catsOrder[i].sesso;
+  // }
+  // console.log(catsOrder);
 
 });
